fed9dd88c475777000109dc9cc605a93b5ab262b
Mehdi Cheracher <mehdi.cheracher@oracle.com>
2019-10-22 15:49:14 +0100

    initial commit, repository setup

---
+++ backend/src/main/java/ma/avito/usere/UserEditorApiApplication.java
@@ -0,0 +1,11 @@
+package ma.avito.usere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserEditorApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserEditorApiApplication.class, args);
+    }
+}
